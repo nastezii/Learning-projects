479d86d1a3530edf12ce67560db32e6beac23e59
Shalak Anastasia <nastiashalak@gmail.com>
2024-12-15 00:01:36 +0200

    class "OrderManagement" added

--- cafe order management/Program.cs
+++ cafe order management/Program.cs
@@ -3,6 +3,131 @@
 
 namespace CafeOrderManagament
 {
+    public enum OrderStatus
+    {
+        Pending,
+        InProgress,
+        Completed,
+        Cancelled
+    }
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public MenuItem(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public List<KeyValuePair<string, float>> Details = new List<KeyValuePair<string, float>>() ;
+        public float Amount { get; set; }
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+    }
+
+    public class OrderManagement
+    { 
+        private List<Order> orders = new List<Order>();
+        private int lastOrderId = 0;
+        private int GenerateId() => lastOrderId++;
+
+        public void AddOrder(string customerName, List<KeyValuePair<string, float>> orderDetails, float orderAmount)
+        { 
+            var order = new Order();
+            order.CustomerName = customerName;
+            order.Id = GenerateId();
+            order.Details = orderDetails;
+            order.Amount = orderAmount;
+
+            orders.Add(order);
+            Console.WriteLine("Order successfully added.");
+        }
+
+        public void UpdateOrderStatus(int orderId, OrderStatus newStatus)
+        {
+            var order = orders.FirstOrDefault(order => order.Id == orderId);
+            if (order != null)
+            {
+                order.Status = newStatus;
+                Console.WriteLine("Status of order successfully changed.");
+            }
+            else 
+            {
+                Console.WriteLine("Order not found.");
+            }
+            if (order.Status == OrderStatus.Cancelled)
+            {
+                orders.Remove(order);
+            }
+        }
+
+        public void FilteringByName(string customer)
+        {
+            List<Order> customerOrders = new List<Order>();
+            foreach (var order in orders)
+            {
+                if (order.CustomerName == customer)
+                { 
+                    customerOrders.Add(order);
+                }
+            }
+            if (customerOrders.Count != 0)
+            {
+                Console.WriteLine("There is no such customer in the database.");
+            }
+            else
+            {
+                Console.WriteLine($"{customer} orders :");
+                foreach (var order in customerOrders)
+                {
+                    Console.WriteLine($"Order ID: {order.Id}, date: {order.CreationTime}, status: {order.Status}.");
+                }
+            }
+        }
+
+        public void FilteringByStatus(OrderStatus status)
+        {
+            List<Order> ordersByStatus = new List<Order>();
+            foreach (var order in orders)
+            {
+                if (order.Status == status)
+                {
+                    ordersByStatus.Add(order);
+                }
+            }
+            if (ordersByStatus.Count != 0)
+            {
+                Console.WriteLine("There are currently no orders with this status.");
+            }
+            else
+            {
+                Console.WriteLine($"Orders by status \"{status}\":");
+                foreach (var order in ordersByStatus)
+                {
+                    Console.WriteLine($"Customer name: {order.CustomerName}, order ID: {order.Id}, date: {order.CreationTime}.");
+                }
+            }
+        }
+
+        public float AmountCalculating()
+        {
+            float sum = 0;
+            foreach (var order in orders)
+            {
+                foreach (var dishes in order.Details)
+                {
+                    sum += dishes.Value;
+                }
+            }
+            return sum;
+        }
+    }
 
     public class MenuManagement
     {
@@ -13,7 +138,6 @@ namespace CafeOrderManagament
             if (MenuItemExists(name))
             {
                 Console.WriteLine("A menu item with that name already exists. New item not added.");
-                return;
             }
             else
             {
@@ -33,7 +157,6 @@ namespace CafeOrderManagament
             else
             {
                 Console.WriteLine("A menu item with that name do not exists.");
-                return;
             }
         }
 
@@ -42,7 +165,6 @@ namespace CafeOrderManagament
             if (menu.Count == 0)
             {
                 Console.WriteLine("There is no item in the menu.");
-                return;
             }
             else
             {
@@ -59,35 +181,4 @@ namespace CafeOrderManagament
             return menu.Any(item => item.Name == name);
         }
     }
-
-
-    public class MenuItem
-    {
-        public string Name { get; set; }
-        public float Price { get; set; }
-        public MenuItem(string name, float price)
-        {
-            Name = name;
-            Price = price;
-        }
-    }
-
-    public class Order
-    {
-        public int OrderNumber { get; set; }
-        public string CustomerName { get; set; }
-        public List<string> OrderDetails { get; set; }
-        public float OrderAmount { get; set; }
-        public DateTime OrderCreationTime { get; set; }
-        public string OrderStatus { get; set; }
-    }
-
-    enum OrderStatus
-    {
-        Pending,
-        InProgress,
-        Completed,
-        Cancelled
-    }
-
 }
