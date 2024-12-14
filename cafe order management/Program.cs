93349b76112d2eb1cddda7e391f42ba28efe209d
Shalak Anastasia <nastiashalak@gmail.com>
2024-12-14 13:59:41 +0200

    task started

---
+++ cafe order management/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+public struct Order
+{
+    public int OrderNumber { get; set; }
+    public string CustomerName { get; set; }
+    public List<string> OrderDetails { get; set; }
+    public float OrderAmount { get; set; }
+    public DateTime OrderCreationTime { get; set; }
+    public string OrderStatus { get; set; }
+}
+
+
+enum OrderStatus
+{
+    Pending, 
+    InProgress, 
+    Completed, 
+    Cancelled
+}
