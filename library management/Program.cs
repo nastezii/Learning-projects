0affc8f30a8f2c8538a2db8ee44cf3e7cba920a2
Shalak Anastasia <nastiashalak@gmail.com>
2024-12-14 13:10:36 +0200

    Add project files.

---
+++ library management/Program.cs
@@ -0,0 +1,15 @@
+﻿class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+    public Genre Genre { get; set; }
+}
+
+enum Genre
+{
+    fiction,
+    nonFiction,
+    science,
+    history
+}
